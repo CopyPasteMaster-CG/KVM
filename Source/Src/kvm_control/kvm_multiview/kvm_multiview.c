f65c52478c2c0952c5175c7695aa6d0be93e36c0
CopyPasteMaster <2298091947@qq.com>
2026-07-01 09:22:18 +0800

    调试自定义串口，修改串口通信的宏定义

--- Source/Src/kvm_control/kvm_multiview/kvm_multiview.c
+++ Source/Src/kvm_control/kvm_multiview/kvm_multiview.c
@@ -48,7 +48,7 @@ U16_T  MULTIVIEW_RxCount = 0;
 U16_T  MULTIVIEW_RxTail_Hold=0;
 
 static KVM_RX_DATA kvm_rx;
-#define MULTIVIEW_UART_DEBUG 0
+#define MULTIVIEW_UART_DEBUG 1
 
 U8_T MULTIVIEW_Cmd[7][8] =
 {
@@ -117,24 +117,6 @@ U8_T KVM_CRC8_Calculate(const U8_T *crcdata, U8_T length)
     return crc;
 }
 
-#define KVM_MODE_PORT1_ONLY              0x01
-#define KVM_MODE_PORT2_ONLY              0x02
-#define KVM_MODE_PORT3_ONLY              0x03
-#define KVM_MODE_PORT4_ONLY              0x04
-
-#define KVM_MODE_TWO_SMALL_MAIN_PORT1    0x05
-#define KVM_MODE_TWO_SMALL_MAIN_PORT2    0x06
-#define KVM_MODE_TWO_SMALL_MAIN_PORT3    0x07
-
-#define KVM_MODE_PORT12_ONLY             0x08
-#define KVM_MODE_PORT34_ONLY             0x09
-#define KVM_MODE_PORT_ALL                0x0A
-#define KVM_MODE_PORT_ALL_SYNC           0x0B
-
-#define KVM_MODE_THREE_SMALL_MAIN_PORT4  0x0C
-#define KVM_MODE_THREE_SMALL_MAIN_PORT3  0x0D
-#define KVM_MODE_THREE_SMALL_MAIN_PORT2  0x0E
-#define KVM_MODE_THREE_SMALL_MAIN_PORT1  0x0F
 
 void KVM_SET_mode(U8_T mode){
 
